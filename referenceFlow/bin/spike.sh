870434214725f84ca8f7b34ef5e231caf3ba6f40
Efthymios Topalidis <55020988+EfTop27@users.noreply.github.com>
2024-07-14 19:36:49 +0300

    Look only for local spike build
    Signed-off-by: Efthymios Topalidis <55020988+EfTop27@users.noreply.github.com>

--- referenceFlow/bin/spike.sh
+++ referenceFlow/bin/spike.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-if which spike > /dev/null 2>&1; then
-    echo "Using `which spike`"
-    spike $@
-else
-    BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-    echo "Using $BINDIR/spike"
-    $BINDIR/spike $@
-fi
+
+BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo "Using $BINDIR/spike"
+$BINDIR/spike $@
